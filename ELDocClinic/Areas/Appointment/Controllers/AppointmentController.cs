42dcd3b1893e85a3cd6d7a5535f1a386e7e83700
Moaaz Mostafa <moaazmostafa7474@gmail.com>
2024-03-20 03:34:43 +0200

    Added Appointments Area

---
+++ ELDocClinic/Areas/Appointment/Controllers/AppointmentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELDocClinic.Areas.Appointment.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
